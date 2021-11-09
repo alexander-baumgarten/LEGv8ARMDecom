7a436be463a90251a00cbacd3443ddfc9acad561
Robby Rice <ricerob@iastate.edu>
2021-11-09 14:24:52 -0600

    we got binary

--- Projects/COMS321Decompiler/decom/decom.py
+++ Projects/COMS321Decompiler/decom/decom.py
@@ -3,4 +3,5 @@ import sys
 if __name__ == "__main__":
     print("In main")
     f = open(sys.argv[1], "rb")
-    print(f.read())
+    #print(sys.argv)
+    print(bin(int.from_bytes(f.read(4), byteorder='big')))
