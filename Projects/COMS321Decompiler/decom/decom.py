b8967c546470616389614774ac5deb762c7e9bff
Robby Rice <ricerob@iastate.edu>
2021-11-15 13:48:06 -0600

    R type instructions done - should be easily replicated for other types

--- Projects/COMS321Decompiler/decom/decom.py
+++ Projects/COMS321Decompiler/decom/decom.py
@@ -1,32 +1,34 @@
 import sys
 
+### Denotes done
+
 instruction_dict = [
-  ["ADD", 0b10001011000, "R"],
+  ["ADD", 0b10001011000, "R"], ###
   ["ADDI", 0b1001000100, "I"],
-  ["AND", 0b10001010000, "R"],
+  ["AND", 0b10001010000, "R"], ###
   ["ANDI", 0b1001001000, "I"],
   ["B", 0b000101, "B"],
   ["BL", 0b100101, "B"],
-  ["BR", 0b11010110000, "R"],
+  ["BR", 0b11010110000, "R"], ###
   ["CBNZ", 0b10110101, "CB"],
   ["CBZ", 0b10110100, "CB"],
   ["DUMP", 0b11111111110, "DUMP"],
-  ["EOR", 0b11001010000, "R"],
+  ["EOR", 0b11001010000, "R"], ###
   ["EORI", 0b1101001000, "I"],
   ["HALT", 0b11111111111, "HALT"],
   ["LDUR", 0b11111000010, "D"],
-  ["LSL", 0b11010011011, "R"],
-  ["LSR", 0b11010011010, "R"],
-  ["ORR", 0b10101010000, "R"],
+  ["LSL", 0b11010011011, "R"], ###
+  ["LSR", 0b11010011010, "R"], ###
+  ["ORR", 0b10101010000, "R"], ###
   ["ORRI", 0b1011001000, "I"],
   ["PRNL", 0b11111111100, "PRNL"],
   ["PRNT", 0b11111111101, "PRNT"],
   ["STUR", 0b11111000000, "D"],
-  ["SUB", 0b11001011000, "R"],
+  ["SUB", 0b11001011000, "R"], ###
   ["SUBI", 0b1101000100, "I"],
   ["SUBIS", 0b1111000100, "I"],
-  ["SUBS", 0b11101011000, "R"],
-  ["MUL", 0b10011011000, "R"]
+  ["SUBS", 0b11101011000, "R"], ###
+  ["MUL", 0b10011011000, "R"] ###
 ]
 
 def decodeBytes(current_byte):
@@ -37,14 +39,55 @@ def decodeBytes(current_byte):
             return instruction
     return "ERR"
 
-def decodeRInstruction(instruction):
-    print(instruction[2:])
+def decodeRInstruction(instruction, instruction_info):
     opcode = instruction[2:13]
     Rm = instruction[13:18]
     shamt = instruction[18:24]
     Rn = instruction[24:29]
     Rd = instruction[29:]
     print(opcode + " " + Rm + " " + shamt + " " + Rn + " " + Rd)
+    if instruction_info[0] == "ADD" or "AND" or "EOR" or "ORR" or "SUB" or "SUBS" or "MUL":
+        print(instruction_info[0] + " X" + str(int(Rd, 2)) + ", X" + str(int(Rn, 2)) + ", X" + str(int(Rm, 2)))
+    elif instruction_info[0] == "BR":
+        print(instruction_info[0] + " X" + str(int(Rd, 2)))
+    elif instruction_info[0] == "LSL" or "LSR":
+        print(instruction_info[0] + " X" + str(int(Rd, 2)) + ", X" + str(int(Rn, 2)) + ", #" + str(int(shamt)))
+    else:
+        print("Error decoding R type instruction. Instruction data: " + instruction_info +
+              ". Instruction binary: " + instruction)
+
+
+def decodeIInstruction(instruction, instruction_info):
+    opcode = instruction[2:12]
+    ALU_immediate = instruction[12:23]
+    Rn = instruction[23:29]
+    Rd = instruction[29:]
+
+def decodeDInstruction(instruction, instruction_info):
+    print("In decodeD")
+
+def decodeBInstruction(instruction, instruction_info):
+    print("In decodeB")
+
+
+def decodeCBInstruction(instruction, instruction_info):
+    print("In decodeCB")
+
+
+def decodeDUMPInstruction(instruction, instruction_info):
+    print("In decodeDUMP")
+
+
+def decodeHALTInstruction(instruction, instruction_info):
+    print("In decodeHALT")
+
+
+def decodePRNLInstruction(instruction, instruction_info):
+    print("In decodePRNL")
+
+
+def decodePRNTInstruction(instruction, instruction_info):
+    print("In decodePRNT")
 
 
 if __name__ == "__main__":
@@ -59,25 +102,23 @@ if __name__ == "__main__":
                 print("ERROR: Failure reading opcode. Result: " + instruction + ". Input: " + current_byte)
                 exit()
         if instruction[2] == "R":
-            decodeRInstruction(current_byte)
-            #g = str(current_byte)[2:13]
-            #print(g)
+            decodeRInstruction(current_byte, instruction)
         elif instruction[2] == "I":
-            print("I")
+            decodeIInstruction(current_byte, instruction)
         elif instruction[2] == "D":
-            print("D")
+            decodeDInstruction(current_byte, instruction)
         elif instruction[2] == "B":
-            print("B")
+            decodeBInstruction(current_byte, instruction)
         elif instruction[2] == "CB":
-            print("CB")
+            decodeCBInstruction(current_byte, instruction)
         elif instruction[2] == "DUMP":
-            print("DUMP")
+            decodeDUMPInstruction(current_byte, instruction)
         elif instruction[2] == "HALT":
-            print("HALT")
+            decodeHALTInstruction(current_byte, instruction)
         elif instruction[2] == "PRNL":
-            print("PRNL")
+            decodePRNLInstruction(current_byte, instruction)
         elif instruction[2] == "PRNT":
-            print("PRNT")
+            decodePRNTInstruction(current_byte, instruction)
         else:
             print("ERROR: Failure in reading instruction class. Instruction: " + instruction[0] + ". Type: " + instruction[2])
             exit(1)
